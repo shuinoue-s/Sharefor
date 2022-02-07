61a10f97edb5b01fa1cdea8df7d78a7d7b1e98c8
shuinoue-s <i.shushu0803@gmail.com>
2022-02-07 10:48:04 +0900

    Add: 未認証時には投稿フォームを表示させないように変更
    未認証時にも投稿フォームが表示されていたため。

--- src/App.vue
+++ src/App.vue
@@ -3,31 +3,24 @@
     <app-header/>
       <v-main>
         <router-view/>
-        <Form/>
+        <Form v-if="isAuthenticated" />
       </v-main>
   </v-app>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 import AppHeader from './components/AppHeader'
 import Form from './views/Form'
 
 export default {
-  
   name: 'App',
-
-  data() {
-    return {
-
-    }
-  },
   components: {
     AppHeader,
     Form
-
+  },
+  computed: {
+    ...mapGetters('auth', ['isAuthenticated'])
   }
 }
 </script>
-
-<style>
-</style>
